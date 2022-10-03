766d1ace1ba2d8a1c0f1102909f4760f0b9e6d8b
SolovyevCoding <104126292+SolovyevCoding@users.noreply.github.com>
2022-10-03 18:37:19 +0300

    Добавил контроллер

---
+++ FitnessApp.BL/Controller/UserController.cs
@@ -0,0 +1,32 @@
+﻿using FitnessApp.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.BL.Controller
+{
+    public class UserController
+    {
+        public User User { get; }
+
+        public UserController(User user)
+        {
+            User = user ?? throw new ArgumentNullException("Поле пользователь введено неккоректно", nameof(user));
+        }
+
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, User);
+                
+            } 
+        }
+    }
+}
